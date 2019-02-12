8acef2e37911c1e23de48dc6bc18e0c7b3d08d34
Mo Khaial <mkhaial@terpmail.umd.edu>
2019-02-12 12:07:36 -0500

    ENAE484Legs

--- Structures/Calculations/ENAE484Legs.m
+++ Structures/Calculations/ENAE484Legs.m
@@ -14,9 +14,10 @@ g = 1.62;
 
 % velocity at touchdown (m/s)
 v = 2;
-
+% time after touchdown (s)
+t = 1;
 %impulse (N-s)
-I = mtot*v;
+%I = mtot*v;
 
 %weight
 W = mtot*g;
@@ -29,19 +30,19 @@ Ke = 1/2*mtot*v^2;
 %ri = 0
 
 %diameter and thickness for a circular ring
-d = .1:.01:2;
-t = .2;
+do = .1:.01:2;
+di = do-.2;
 
 % use area of an annulus (ring)
 %A = pi*(ro^2 - ri^2);
-A = pi.*d.*t; % circular ring
+A = pi/4.*(do.^2-di.^2); % circular ring
 
 %moment of intertia of a ring (kg-m^2)
 %density = 2700; % kg/m^3
 %volume = pi*L*(ro^2-ri^2);
 %m = 500/4 %(kg)
 %I = 1/2*m*(ro^2 + ri^2);
-I = (pi.*d.^3.*t)./8./3;
+I = pi/64.*(do.^4-di.^4);
 
 %E for 6061 aluminum (Pa)
 E = 68.9e9;
@@ -49,7 +50,7 @@ E = 68.9e9;
 % yield strength (sigma) (Pa)
 %O = Pcr/A
 O = 276e6;
-Pcr = O.*A;
+Pcr = mtot*v/t;
 
 %buckling (using aluminum)
 %Pcr = (pi^2*E*I)/(KL)^2
@@ -57,7 +58,7 @@ K = .5; % (fixed-fixed)
 L = sqrt(((pi^2)*E.*I)./(Pcr.*K^2));
 
 for i = 1:length(L)
-    if L(i) < 5
+    if L(i) < 10
         Ln(1,i) = L(i);
         Ln(2,i) = i;
     end
